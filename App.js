3427b3a5dd26a26be72e452a8f42d42fc3433188
ntolstikova <n.tolstikova@aimprosofrt.com>
2020-02-13 13:13:30 +0200

    init

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => (
+  <View style={styles.primary}>
+    <Text>qweqwe</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  primary: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+  },
+});
+
+export default App;
